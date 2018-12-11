7e29f54f012f9e1b6db2f7ff47216ce7755188ed
Cory Kim <corykim0829@gmail.com>
2018-12-12 05:26:24 +0900

    Initial Commit

---
+++ OldYouTubeTests/OldYouTubeTests.swift
@@ -0,0 +1,34 @@
+//
+//  OldYouTubeTests.swift
+//  OldYouTubeTests
+//
+//  Created by Cory Kim on 12/12/2018.
+//  Copyright © 2018 CoryKim. All rights reserved.
+//
+
+import XCTest
+@testable import OldYouTube
+
+class OldYouTubeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
